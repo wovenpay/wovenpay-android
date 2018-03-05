9c72e7db318c0da361568a7313b1b80b97fffb08
makunomark <makunomark@gmail.com>
2018-03-05 11:09:08 +0300

    initial commit

---
+++ app/src/main/java/com/wovenpay/wovenpay/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wovenpay.wovenpay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
